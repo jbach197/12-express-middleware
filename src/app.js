dbc3b9bc6b5fb2840efc37e9f3ae66c29f07ca63
Jennifer Bach <jmbach1@gmail.com>
2018-05-29 16:32:04 -0700

    updates

---
+++ src/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import express from 'express';
+let app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+import router from './api/api.js';
+app.use( router );
+
+let running = false;
+
+module.exports = {
+  start: (port) => {
+    if(! running) {
+      app.listen(port, (err) => {
+        if(err) { throw err; }
+        running = true;
+        console.log('Server is up on port', port);
+      });
+    }
+    else {
+      console.log('Server is already running');
+    }
+  },
+
+  stop: () => {
+    app.close( () => {
+      running = false;
+      console.log('Server has been stopped');
+    });
+  },
+};
